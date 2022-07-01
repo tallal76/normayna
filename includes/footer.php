d569f2fd545c16bed4b86187b9340ad2309afcfe
BouazziH <104574763+BouazziH@users.noreply.github.com>
2022-07-01 23:10:22 +0200

    adding the link of the page mention legale

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
 <footer>
-    <p>&copy; <?php echo date("Y"); ?> - NORMAYNA Corporation</p>
+    <p>&copy; <?php echo date("Y"); ?> - <a href="index.php?page=mentionLegale">NORMAYNA Corporation</a> </p>
 </footer>
 </body>
 </html>
