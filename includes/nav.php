54cae866ba6e187ae62cbab3702a2981ce36808b
BouazziH <104574763+BouazziH@users.noreply.github.com>
2022-07-04 22:27:05 +0200

    update some code on those 3 pages

--- includes/nav.php
+++ includes/nav.php
@@ -1,5 +1,5 @@
 <header>
-    <nav>
+    <!--    <nav>
         <div class="logo">
             <a href="index.php?page=accueil"><img src="./assets/img/normaynawhite.png" alt="logo"></a>
         </div>
@@ -12,5 +12,32 @@
 
             </ul>
         </div>
-    </nav>
+ </nav> -->
+    <nav class="navbar light-mode" role="navigation" >
+    <div class="navbar__logo"><a href="index.php?page=accueil"><img src="./assets/img/normaynawhite.png" alt="logo"></a></div>    
+    <ul class="navbar__links">
+      <li class="navbar__link first"><a href="index.php?page=accueil">Accueil</a></li>
+      <li class="navbar__link second"><a href="index.php?page=carte">La carte</a></li>
+      <li class="navbar__link third"><a href="index.php?page=evenement">Evenements</a></li>
+      <li class="navbar__link four"><a href="index.php?page=apropos">Apropos</a></li>
+    
+    </ul>   
+    <button class="burger">
+      <span class="bar"></span>  
+    </button>   
+  </nav> 
+
+
+<!--     <div class="topnav" id="myTopnav">
+        <ul class="navbar__links">
+            <li class="navbar__link first"><a href="index.php?page=accueil">Accueil</a></li>
+            <li class="navbar__link second"><a href="index.php?page=carte">La carte</a></li>
+            <li class="navbar__link third"><a href="index.php?page=evenement">Evenements</a></li>
+            <li class="navbar__link four"><a href="index.php?page=apropos">Apropos</a></li>
+            <li></li>
+        </ul>
+        <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+                    <i class="fa fa-bars"></i></a>
+        
+    </div> -->
 </header>
