1950b7b58cc2cbe188a0fec9779798981e3b3915
Sonia Chalal <contact@soniawebdesigner.fr>
2022-07-05 11:12:48 +0200

    création du footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,82 @@
-<footer>
-    <p>&copy; <?php echo date("Y"); ?> - NORMAYNA Corporation</p>
+<div id="maps" class="map-responsive">
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2599.5793490066003!2d1.094897915853499!3d49.34118227454453!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e1206042f7421d%3A0x1967a397a73d6b9b!2s24%20Rue%20du%20Mar%C3%A9chal%20Foch%2C%2076350%20Oissel!5e0!3m2!1sfr!2sfr!4v1656940674629!5m2!1sfr!2sfr" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+</div>
+<div id="top-footer" class="">
+  <div class="clearfix">
+    <div id="info">
+      Passez votre commande au<strong> 06 74 30 72 06 - Repas à emporter</strong>
+    </div>
+  </div>
+</div>
+<footer class="footer">
+
+  <div class="footer__addr">
+   <h1 class="footer__logo">
+    <img src="./assets/img/favicon-144.png" alt="">
+    </h1>
+     <address>
+         <h2><strong>06 74 30 72 06</strong></h2>
+        <br>
+        23 rue andré Callac<br>76350 OISSEL
+        <a class="footer__btn" href="mailto:example@gmail.com">Contactez nous</a>
+    </address>
+  </div>
+
+  <ul class="footer__nav">
+    <li class="nav__item">
+      <h2 class="nav__title"><strong> La Carte</strong></h2>
+
+      <ul class="nav__ul">
+        <li>
+          <a href="#">Couscous</a>
+        </li>
+        <li>
+          <a href="#">Pastilla</a>
+        </li>
+        <li>
+          <a href="#">Msemen</a>
+        </li>
+
+        <li>
+          <a href="#">Msemen farcie</a>
+        </li>
+        <li>
+          <a href="#">Pâtisserie orientale</a>
+        </li>
+      </ul>
+    </li>
+
+    <li class="nav__item">
+      <h2 class="nav__title"><strong>Legal</strong></h2>
+
+      <ul class="nav__ul">
+        <li>
+          <a href="#">Privacy Policy</a>
+        </li>
+
+        <li>
+          <a href="#">Terms of Use</a>
+        </li>
+
+        <li>
+          <a href="#">Sitemap</a>
+        </li>
+      </ul>
+    </li>
+    <li class="nav__item nav__item--extra">
+      <h2 class="nav__title">
+        <strong>Horaires</strong></h2>
+        <p>
+            <strong>Lundi au vendredi</strong><br>
+            12:00-14:00 | 19:00-21:30 <br>
+            <strong>Samedi - dimanche</strong><br>
+            12:00-14:00 | 19:00-22:00
+        </p>
+
+    </li>
+  </ul>
+
+  <div class="legal">
+    <p>&copy; <?php echo date("Y"); ?> - O DELICE DE NORMAYNA</p>
+  </div>
 </footer>
-</body>
-</html>
--- includes/head.php
+++ includes/head.php
@@ -4,9 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" media="screen" href="./assets/css/style.css" />
-   <!--   <link rel="stylesheet" media="screen" href="./assets/css/sliderAccueil.css" />  -->
-    <link rel="stylesheet"  href="https://unpkg.com/swiper@8/swiper-bundle.min.css"/>
+   
+    <link rel="stylesheet"  href="./assets/css/style.css" />
+    <!--   <link rel="stylesheet" media="screen" href="./assets/css/sliderAccueil.css" />  -->
+    <link rel="stylesheet"  href="https://unpkg.com/swiper@8/swiper-bundle.min.css"/> 
+    <script src="https://use.fontawesome.com/6fd70f3f24.js"></script>
+
     <title>Normayna</title>
   
 </head>
