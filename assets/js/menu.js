01b44c2e5cddec21b21d3339aae219413f2d76bb
BouazziH <bouazzicdi@gmail.com>
2022-07-05 10:35:52 +0200

    le code js fixer le probleme falait ajouter un [0]

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,16 +1,8 @@
- function toggleMenu () {  
-    const navbar = document.getElementById(".navbar");
-    const burger = document.getElementById(".burger");
-    burger.addEventListener('click', (e) => {
+
+     function show() {
+      const navbar = document.getElementsByClassName("navbar")[0];
+      console.dir(navbar);
+      console.log('click');
       navbar.classList.toggle('show-nav');
-    })   
-  }
-  toggleMenu(); 
-/*   function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  } */
+    } 
+ 
