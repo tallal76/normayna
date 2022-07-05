47a530040318c693ae658bbc613fd0f2306cad93
BouazziH <bouazzicdi@gmail.com>
2022-07-05 10:32:26 +0200

    le code tml final pour la nav et l'ajout du nom de fonction sur le boutton

--- includes/nav.php
+++ includes/nav.php
@@ -1,43 +1,18 @@
 <header>
-    <!--    <nav>
-        <div class="logo">
-            <a href="index.php?page=accueil"><img src="./assets/img/normaynawhite.png" alt="logo"></a>
-        </div>
-        <div class="menu">
-            <ul>
-                <li><a href="index.php?page=accueil">Accueil</a></li>
-                <li><a href="index.php?page=carte">La carte</a></li>
-                <li><a href="index.php?page=evenement">Evenements</a></li>
-                <li><a href="index.php?page=apropos">Apropos</a></li>
 
-            </ul>
-        </div>
- </nav> -->
-    <nav class="navbar light-mode" role="navigation" >
-    <div class="navbar__logo"><a href="index.php?page=accueil"><img src="./assets/img/normaynawhite.png" alt="logo"></a></div>    
+  <nav class="navbar dark-mode" role="navigation">
+    <div class="navbar__logo"><a href="index.php?page=accueil"><img src="./assets/img/normaynawhite.png" alt="logo"></a></div>
     <ul class="navbar__links">
       <li class="navbar__link first"><a href="index.php?page=accueil">Accueil</a></li>
       <li class="navbar__link second"><a href="index.php?page=carte">La carte</a></li>
       <li class="navbar__link third"><a href="index.php?page=evenement">Evenements</a></li>
-      <li class="navbar__link four"><a href="index.php?page=apropos">Apropos</a></li>
-    
-    </ul>   
-    <button class="burger">
-      <span class="bar"></span>  
-    </button>   
-  </nav> 
+      <li class="navbar__link four"><a href="index.php?page=apropos">A propos</a></li>
+
+    </ul>
+    <button class="burger" onclick="show()">
+      <span class="bar"></span>
+    </button>
+  </nav>
 
 
-<!--     <div class="topnav" id="myTopnav">
-        <ul class="navbar__links">
-            <li class="navbar__link first"><a href="index.php?page=accueil">Accueil</a></li>
-            <li class="navbar__link second"><a href="index.php?page=carte">La carte</a></li>
-            <li class="navbar__link third"><a href="index.php?page=evenement">Evenements</a></li>
-            <li class="navbar__link four"><a href="index.php?page=apropos">Apropos</a></li>
-            <li></li>
-        </ul>
-        <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-                    <i class="fa fa-bars"></i></a>
-        
-    </div> -->
 </header>
