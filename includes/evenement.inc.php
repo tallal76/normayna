8b4d60dae423c63ad13e45afc94651e08a12e940
BouazziH <bouazzicdi@gmail.com>
2022-08-26 10:59:59 +0200

    modifcation des requettes pour filter en date

--- includes/evenement.inc.php
+++ includes/evenement.inc.php
@@ -8,62 +8,62 @@
 
     <?php
 
-if(isset($_GET['link'])){
-  $var = $_GET['link'];
-  if($var == "futur"){
-    $requette = "select theme_event,adress_event,
+  if (isset($_GET['link'])) {
+    $var = $_GET['link'];
+    if ($var == "futur") {
+      $requette = "select theme_event,adress_event,
     DATE_FORMAT(date_event, '%d/%m/%Y %H:%i:%s') as date_event ,description_event,video_event from evenements where date_event >NOW() ;";
-  }elseif($var == "today"){
-    $requette = "select theme_event,adress_event,
-    DATE_FORMAT(date_event, '%d/%m/%Y %H:%i:%s') as date_event ,description_event,video_event from evenements where date_event = SELECT DATE( NOW() );";
-  }elseif($var == "past"){
-    $requette = "select theme_event,adress_event,
+    } elseif ($var == "today") {
+      $requette = "select theme_event,adress_event,
+    DATE_FORMAT(date_event, '%d/%m/%Y %H:%i:%s') as date_event ,description_event,video_event from evenements where date(date_event) = CURRENT_DATE();";
+    } elseif ($var == "past") {
+      $requette = "select theme_event,adress_event,
     DATE_FORMAT(date_event, '%d/%m/%Y %H:%i:%s') as date_event ,description_event,video_event from evenements where date_event  < NOW();";
-}}
-else{
-  
-  $requette = "SELECT theme_event,adress_event,
+    }
+  } else {
+
+    $requette = "SELECT theme_event,adress_event,
    DATE_FORMAT(date_event, '%d/%m/%Y %H:%i:%s') as date_event ,description_event,video_event FROM evenements;";
-  //$requette = "select * from evenements  ;";
+    //$requette = "select * from evenements  ;";
   }
-//$requette = "select * from evenements  ;";//pour le teste if faut changer la date NOW() ou une date > '2022-07-22';
-$cnx = new sql();
-$requette = $cnx->afficher($requette);
-if (isset($requette) & !empty($requette)) {
+  //$requette = "select * from evenements  ;";//pour le teste if faut changer la date NOW() ou une date > '2022-07-22';
+  $cnx = new sql();
+  $requette = $cnx->afficher($requette);
+  if (isset($requette) & !empty($requette)) {
     echo '<div class="container">';
     echo '<div class="box">';
-    
+
     echo '<table border="3">';
     echo '<tr>';
     echo '<td>theme_event</td>';
     echo '<td>adress_event</td>';
-    
+
     echo '<td>date_event</td>';
     echo '<td>description_event</td>';
     echo '<td>video_event</td>';
-    
+
     echo '</tr>';
-    
+
     foreach ($requette as $key => $value) {
-    
-    
-        echo '<tr>';
-        echo '<td>' . $value['theme_event'] . '</td>';
-        echo '<td>' . $value['adress_event'] . '</td>';
-        echo '<td>' . $value['date_event'] . '</td>';
-        echo '<td>' . $value['description_event'] . '</td>';
-       echo '<td>' . $value['video_event'] . '</td>';
-       
-     /*    echo '<td> <input type="submit" name="submit" value="Uploader" /></td>'; */
-        echo '</tr>';
+
+
+      echo '<tr>';
+      echo '<td>' . $value['theme_event'] . '</td>';
+      echo '<td>' . $value['adress_event'] . '</td>';
+      echo '<td>' . $value['date_event'] . '</td>';
+      echo '<td>' . $value['description_event'] . '</td>';
+      echo '<td>' . $value['video_event'] . '</td>';
+
+      /*    echo '<td> <input type="submit" name="submit" value="Uploader" /></td>'; */
+      echo '</tr>';
     }
-    
+
     echo '</table>';
     echo '</div>';
     echo '</div>';
-}else{
+  } else {
     echo " <span class='waring'><strong>!!!!!! il n'ya pas d'evenement</strong></span>";
-}
+  }
 
-?>
+  ?>
 </div>
