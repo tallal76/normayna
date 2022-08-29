0c401276c9e2dae77475cc50238127fe1f626b57
BouazziH <104574763+BouazziH@users.noreply.github.com>
2022-08-29 22:29:19 +0200

    modfication du href du footer pour la index de la page mention legale

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
 <footer>
-    <p>&copy; <?php echo date("Y"); ?> - <a href="index.php?page=mentionLegale">NORMAYNA Corporation</a> </p>
+    <p>&copy; <?php echo date("Y"); ?> - <a href="index.php?page=MentionLegale">NORMAYNA Corporation</a> </p>
 </footer>
 </body>
 </html>
