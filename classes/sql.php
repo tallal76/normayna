1017929131bddf666e3fc781c351529e6df3589a
BouazziH <104574763+BouazziH@users.noreply.github.com>
2022-09-02 12:05:51 +0200

    adding new function on classes

--- classes/sql.php
+++ classes/sql.php
@@ -26,6 +26,10 @@ class sql
     {
         $this->connexion->exec($query);
     }
+    public function recup(string $query): array
+    {
+        return $this->connexion->query($query)->fetchAll();
+    }
     public function supprimer($query, $idUser)
     {
         $resultat = $this->connexion->prepare($query);
