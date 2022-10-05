d5bb41a8e119b30e44eba5d7e54f83e3e1644204
BouazziH <104574763+BouazziH@users.noreply.github.com>
2022-10-05 11:25:40 +0200

    Modification de ajjout d'un nouveau champ

--- includes/frmProduit.php
+++ includes/frmProduit.php
@@ -9,29 +9,28 @@
         <input type="text" name="prix" id="prix" value="<?= $prix ?>" />
     </div>
     <div>
-    <label for="file">Image :</label>
+        <label for="file">Image :</label>
         <input type="file" name="file" value="<?= $file ?>" />
     </div>
 
-    <select name="repa" >
-<?php
-foreach ($requette as $key => $value){
+    <select name="repa">
+        <?php
+        foreach ($requette as $key => $value) {
+            $nom =   $value['Libelle'];
+            echo "<option value=" . $nom . ">";
+            echo "$nom";
+            echo "</option>";
+        }
+        ?>
 
-
-
-
-   $nom =   $value['Libelle'];
-   
-echo "<option value=".$nom.">";
-echo "$nom";
-echo "</option>";
-}
-?>
-
-</select>
+    </select>
     <div>
+        <div>
+            <label for="description">Description :</label>
+            <textarea name="description" id="description" cols="30" rows="7"></textarea>
+        </div>
         <input type="reset" value="Effacer" />
-        <input type="submit" name ="submit" value="Enregistrer" />
+        <input type="submit" name="submit" value="Enregistrer" />
     </div>
     <input type="hidden" name="frmProduit" />
 </form>
