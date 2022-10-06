f8f59f75f0ac37a9fe054caaf336f5eca08ec857
BouazziH <104574763+BouazziH@users.noreply.github.com>
2022-10-06 13:56:57 +0200

    Some comment

--- classes/sql.php
+++ classes/sql.php
@@ -25,10 +25,10 @@ class sql
     {
         $this->connexion->exec($query);
     }
-    public function recup(string $query): array
+   /*  public function recup(string $query): array
     {
         return $this->connexion->query($query)->fetchAll();
-    }
+    } */
     public function supprimer($query, $idUser)
     {
         $resultat = $this->connexion->prepare($query);
