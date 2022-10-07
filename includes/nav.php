9698872d7bed59b15ab7a52329dcf2d54ad9fd20
BouazziH <104574763+BouazziH@users.noreply.github.com>
2022-10-07 14:43:09 +0200

    nav

--- includes/nav.php
+++ includes/nav.php
@@ -8,7 +8,7 @@
       <li class="navbar__link third"><a href="index.php?page=evenement"> Listes des Evenements</a></li>
       <li class="navbar__link four"><a href="index.php?page=apropos">À propos</a></li>
       <li class="navbar__link four"><a href="index.php?page=ajoutEvenement">Ajouter un event</a></li>
-
+      <li class="navbar__link four"><a href="index.php?page=maj">MAJ</a></li>
       <li class="navbar__link four"><a href="index.php?page=ajoutProduit">Ajouter un produit</a></li>
       <li class="navbar__link four"><a href="index.php?page=categorie">Ajouter une categorie</a></li>
 
