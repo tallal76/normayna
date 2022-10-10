fe932e4e5ae34796e2be4abc93281d771d18fe47
BouazziH <bouazzicdi@gmail.com>
2022-10-10 14:21:26 +0200

    Cleaning code

--- includes/connexion.inc.php
+++ includes/connexion.inc.php
@@ -15,15 +15,12 @@ if (isset($_POST['frmConnexion'])) {
 
     if (count($erreurs)) {
         $messageErreur = "<ul>";
-
         for ($i = 0; $i < count($erreurs); $i++) {
             $messageErreur .= "<li>";
             $messageErreur .= $erreurs[$i];
             $messageErreur .= "</li>";
         }
-
         $messageErreur .= "</ul>";
-
         echo $messageErreur;
         include './includes/frmConnexion.php';
     } else {
@@ -38,35 +35,22 @@ if (isset($_POST['frmConnexion'])) {
                 $nom = $resultatLogin[0]['nom'];
                 $prenom = $resultatLogin[0]['prenom'];
                 if ($t == $resultatPassword) {
-
                     $message = "Vous êtes connecté </br> Bienvenue" . $nom . " " . $prenom;
-
-                   
-
                     $_SESSION['login'] = true;
-
                     $messageEmail = $mail . ' vous êtes connecté !';
-                }
-                //sendEmail($mail, 'contact@ceppic-php-file-rouge.fr', 'Login Success', $messageEmail);
-                else {
+                } else {
                     $message = "Erreur d'authentification";
                     $_SESSION['login'] = false;
                 }
             } else {
                 $message = "Votre adresse n'est pas dans la base";
             }
-
             echo $message;
-
-            // $url = $_SERVER['HTTP_ORIGIN'] . dirname($_SERVER['REQUEST_URI']) . "/";
-
-            //echo redirection($url, 2000);
-            // echo "<p><a href=\"$url\">Revenir à la page d'accueil</a></p>";
+            $url = $_SERVER['HTTP_ORIGIN'] . dirname($_SERVER['REQUEST_URI']) . "/";
+            echo "<p><a href=\"$url\">Revenir à la page d'accueil</a></p>";
         }
     }
 } else {
     $mail = "";
     include './includes/frmConnexion.php';
-
 }
-
