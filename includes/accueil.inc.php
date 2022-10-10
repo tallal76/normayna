f11e5b5add65b1fd69475a7ce2844eb191cba86f
BouazziH <104574763+BouazziH@users.noreply.github.com>
2022-10-10 21:46:12 +0200

    Testing if session exist

--- includes/accueil.inc.php
+++ includes/accueil.inc.php
@@ -1,4 +1,22 @@
+<?php
+if (isset($_SESSION['login'])) {
+    echo "Bienvenue <strong>". $_SESSION['login']."</strong>";
+    ?>
+    <div class="boite">
+    <div class="contenu_carou_auto">
+        <div class="caroussel-image">
+            <img src="./assets/imgslider/briwate.jpg" alt>
+            <img src="./assets/imgslider/couscous-mergez.jpg" alt>
+            <img src="./assets/imgslider/msmen-farsi.jpg" alt>
+            <img src="./assets/imgslider/msmn.jpg" alt>
+            <img src="./assets/imgslider/poulet.jpg" alt>
+            <img src="./assets/imgslider/viande-prune.jpg" alt>
+            <img src="./assets/imgslider/chekhchoukha-plat-algerien.jpeg" alt>
+            <img src="./assets/imgslider/téléchargement.jpg" alt>
+        </div>
+    </div>
 
+<?php } else { ?>
 <div class="boite">
     <div class="contenu_carou_auto">
         <div class="caroussel-image">
@@ -13,3 +31,4 @@
         </div>
     </div>
 
+    <?php } ?>
