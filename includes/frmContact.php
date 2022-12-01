d1508eddf5ae1a884915de6aaf79076876a0d467
BouazziH <104574763+BouazziH@users.noreply.github.com>
2022-12-01 11:26:32 +0100

    Sending mails

---
+++ includes/frmContact.php
@@ -0,0 +1,9 @@
+<form action="contact.php" method="post">
+    Mail: <input type="text" name="mail" value="" />
+    <br />
+    Sujet: <input type="text" name="sujet" value="" />
+    <br />
+    Message: <textarea name="message" cols="40" rows="20"></textarea>
+    <br />
+    <input type="submit" name="envoyer" value="Envoyer" />
+</form>
